797667d12693ee880fdd2eab4219be55e135ae95
dionelo <dionelmartin@gmail.com>
2021-10-12 17:56:29 -0300

    Create sample users in Mongo DB

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/mercadito');
 
 app.get('/api/products', (req, res) => {
 	res.send(data.products);
@@ -16,8 +20,14 @@ app.get('/api/products/:id', (req, res) => {
 	}
 });
 
+app.use('/api/users', userRouter);
+
 app.get('/', (req, res) => {
 	res.send('Server is ready');
+});
+
+app.use((err, req, res, next) => {
+	res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
